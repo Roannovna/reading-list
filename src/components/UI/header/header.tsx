480cc8cc99abfedd30d6269fced08a218acb568e
Roannovna <RoAnna.97line@gmail.com>
2025-09-23 11:20:48 +0300

    feat: background & composition

--- src/components/UI/header/header.tsx
+++ src/components/UI/header/header.tsx
@@ -1,9 +1,7 @@
 export function Header() {
-  return <header>
-    <img 
-      src="/src/assets/icons/logo.svg"
-      alt="Лисичка Ро"
-      width={125}
-    />
-  </header>
+  return (
+    <header>
+      <img src="/src/assets/icons/logo.svg" alt="Лисичка Ро" width={125} />
+    </header>
+  );
 }
--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,19 +1,21 @@
-import { Header } from "../../components/UI/header/header"
-import { BookCard } from "../../components/book-card/book-card"
+import { Header } from "../../components/UI/header/header";
+import { BookCard } from "../../components/book-card/book-card";
+import { BOOKS } from "../../components/book-details-button/book.data";
+import styles from "./home-page.module.css";
 
 function App() {
   return (
-    <div>
-      <header>
+    <div className={styles.body}>
+      <header className={styles.header}>
         <Header />
       </header>
       <main>
-        <BookCard 
-          image="/src/assets/book-cover/sluzhba-dostavki-knig.jpg"
-        />
+        {BOOKS.map((book) => (
+          <BookCard key={book.title + book.id} image={book.cover} />
+        ))}
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
