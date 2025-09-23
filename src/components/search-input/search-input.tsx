b342384533fce1d7b10c803c038319ecc5242a92
Roannovna <RoAnna.97line@gmail.com>
2025-09-23 18:18:41 +0300

    feat: search input

---
+++ src/components/search-input/search-input.tsx
@@ -0,0 +1,20 @@
+import type { ChangeEvent } from "react";
+import styles from "./search-input.module.css";
+
+interface SearchInputProps {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+export function SearchInput(props: SearchInputProps) {
+  return (
+    <div>
+      <input
+        type="search"
+        value={props.value}
+        onChange={props.onChange}
+        placeholder="Search..."
+        className={styles.input}
+      />
+    </div>
+  );
+}
--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,18 +1,41 @@
+import styles from "./home-page.module.css";
 import { Header } from "../../components/UI/header/header";
 import { BookCard } from "../../components/book-card/book-card";
 import { BOOKS } from "../../components/book-details-button/book.data";
-import styles from "./home-page.module.css";
+import { SearchInput } from "../../components/search-input/search-input";
+import { useDebounce } from "../../hooks/useDebounce";
+import { useMemo } from "react";
+import { useState } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const debouncedSearch = useDebounce(searchTerm);
+
+  const books = useMemo(() => {
+    return BOOKS.filter((book) =>
+      book.title.toLowerCase().includes(debouncedSearch.toLowerCase())
+    );
+  }, [debouncedSearch]);
+
   return (
     <div className={styles.body}>
       <header className={styles.header}>
         <Header />
+        <div>
+          <SearchInput
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
       </header>
       <main>
-        {BOOKS.map((book) => (
-          <BookCard key={book.title + book.id} image={book.cover} />
-        ))}
+        {books.length ? (
+          books.map((book) => (
+            <BookCard key={book.title + book.id} image={book.cover} />
+          ))
+        ) : (
+          <p className={styles.result_message}>Book is not found🕯️</p>
+        )}
       </main>
     </div>
   );
